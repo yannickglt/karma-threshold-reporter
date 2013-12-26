7d16d9801108259f982c2e766815b39f025d80a1
Santthosh Selvadurai <santthosh@gmail.com>
2013-12-26 08:59:46 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var os = require('os');
+var path = require('path');
+var fs = require('fs');
+var istanbul = require('istanbul');
+
+var ThresholdReporter = function(baseReporterDecorator, config, logger, helper, formatError) {
+  var log = logger.create('reporter.threshold');
+  var reporterConfig = config.thresholdReporter || {};
+  var pkgName = reporterConfig.suite || '';
+
+  var suites;
+  var pendingFileWritings = 0;
+  var fileWritingFinished = function() {};
+  var allMessages = [];
+
+  baseReporterDecorator(this);
+
+  this.adapters = [function(msg) {
+    allMessages.push(msg);
+  }];
+
+  this.onRunStart = function(browsers) {
+  };
+
+  this.onBrowserStart = function(browser) {
+  };
+
+  this.onBrowserComplete = function(browser) {
+  };
+
+  this.onRunComplete = function() {
+  };
+
+  this.onSpecComplete = function(browser, result) {
+    if (result.coverage) {
+      log.warn('Browser: ' +  browser.id + ', Spec Coverage : ' + result.coverage)
+    }
+  };
+
+  // wait for writing all the xml files, before exiting
+  this.onExit = function(done) {
+
+  };
+};
+
+ThresholdReporter.$inject = ['baseReporterDecorator', 'config', 'logger', 'helper', 'formatError'];
+
+// PUBLISH DI MODULE
+module.exports = {
+  'reporter:threshold': ['type', ThresholdReporter]
+};
