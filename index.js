f5ad5fc6963f69f99179a3913ba268397055c27b
Santthosh Selvadurai <santthosh@gmail.com>
2013-12-27 00:28:29 -0800

    Adding threshold reporter

--- index.js
+++ index.js
@@ -1,51 +1,70 @@
 var istanbul = require('istanbul');
+var util = require('util');
+var istanbulUtils = require('./istanbul-utils');
 
-var ThresholdReporter = function(baseReporterDecorator, config, logger, helper, formatError) {
-  var log = logger.create('reporter.threshold');
-  var reporterConfig = config.thresholdReporter || {};
-
-  var suites;
-  var pendingFileWritings = 0;
-  var fileWritingFinished = function() {};
-  var allMessages = [];
-
+var ThresholdReporter = function(baseReporterDecorator, config, logger, helper) {
+  var log = logger.create('Threshold');
+  var reporterConfig = config.thresholdReporter || { statements: 50, branches: 50, functions: 50, lines: 50};
   baseReporterDecorator(this);
+  var failedExpectation = false;
 
-  this.adapters = [function(msg) {
-    allMessages.push(msg);
-  }];
+  var collector = new istanbul.Collector();
 
-  this.onRunStart = function(browsers) {
-    log.warn('onRunStart');
-  };
+  function lineForKey(summary, key, expectation) {
+    var metrics = summary[key],
+      skipped,
+      result;
+    key = key.substring(0, 1).toUpperCase() + key.substring(1);
+    if (key.length < 12) { key += '                   '.substring(0, 12 - key.length); }
+    result = [ key , ':', metrics.pct + '%', '(', metrics.covered + '/' + metrics.total, ')', 'Threshold : ' + expectation + '%'].join(' ');
+    skipped = metrics.skipped;
+    if(metrics.pct < expectation) {
+      failedExpectation = true;
+    }
 
-  this.onBrowserStart = function(browser) {
-    log.warn('onBrowserStart');
-  };
+    if (skipped > 0) {
+      result += ', ' + skipped + ' ignored';
+    }
+    return result;
+  }
 
-  this.onBrowserComplete = function(browser) {
-    log.warn('onBrowserComplete');
-  };
-
-  this.onRunComplete = function() {
-    log.warn('onRunComplete');
-  };
-
-  this.onSpecComplete = function(browser, result) {
-    log.warn('onSpecComplete');
-    if (result.coverage) {
-      log.warn('Browser: ' +  browser.id + ', Spec Coverage : ' + result.coverage)
+  this.onBrowserComplete = function(browser,result) {
+    if (result && result.coverage) {
+      collector.add(result.coverage);
     }
   };
 
-  // wait for writing all the xml files, before exiting
+  // process the coverage thresholds before exiting
   this.onExit = function(done) {
-    log.warn('onExit');
+    var summaries = [],summary,lines = [],text;
+    collector.files().forEach(function (file) {
+      summaries.push(istanbulUtils.summarizeFileCoverage(collector.fileCoverageFor(file)));
+    });
+
+    summary = istanbulUtils.mergeSummaryObjects.apply(null, summaries);
+    var statements = summary['statements'];
+
+    lines.push('');
+    lines.push('==================== Coverage / Threshold summary =============================');
+    lines.push.apply(lines, [
+      lineForKey(summary, 'statements', reporterConfig.statements),
+      lineForKey(summary, 'branches', reporterConfig.branches),
+      lineForKey(summary, 'functions', reporterConfig.functions),
+      lineForKey(summary, 'lines', reporterConfig.lines)
+    ]);
+    lines.push('================================================================================');
+    text = lines.join('\n');
+    console.log(text);
+
+    if(failedExpectation) {
+      log.error('Failed minimum coverage threshold expectations');
+      process.exit(1);
+    }
     done();
   };
 };
 
-ThresholdReporter.$inject = ['baseReporterDecorator', 'config', 'logger', 'helper', 'formatError'];
+ThresholdReporter.$inject = ['baseReporterDecorator', 'config', 'logger', 'helper'];
 
 // PUBLISH DI MODULE
 module.exports = {
